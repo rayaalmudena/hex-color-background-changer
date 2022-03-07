4472febfb0b51e9982e13ef09f7bc1bcc6c7e42e
rayaalmudena <raya.almudena@gmail.com>
2022-03-07 13:09:17 +0100

    easy buscando ejemplos

--- js/app.js
+++ js/app.js
@@ -1,2 +1,6 @@
 // Implementarsolución aquí
-
+document.querySelector("#btn").addEventListener("click", function(){ 
+    var randomColor = " #" + Math.floor(Math.random()*16777215).toString(16);
+    document.body.style.backgroundColor=randomColor;
+    document.querySelector("#hex-value").innerHTML= randomColor;
+});
